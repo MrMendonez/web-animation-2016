9ec873a1b0da220e667ced3dd7c6f78b5bf4cce2
MrMendonez <dmendonez@gmail.com>
2016-04-20 10:14:32 -0400

    demoed how to transition to next question and how to give player three tries and lose game

---
+++ 04-20-trivia-game-part4-transition-to-next-question/js/scripts.js
@@ -0,0 +1,156 @@
+// Trivia game Psuedo Code
+
+// Welcome Page with msg "Welcome to Disney Trivia"
+// Directions?
+// Three strikes and you lose.
+// The user should type their name.
+// When user clicks button to submit their name the first question appears
+
+$(document).ready(function() {
+
+  var playerName = '';
+  var playerAnswer1 = '';
+  var playerAnswer2 = '';
+  var answer1 = 'snow white'
+  var answer2 = 'walt disney'
+  var correctCounter = 0;
+  var triesLeft = 3
+
+  // Hide Player Name
+  $('#dashboard').hide();
+  $('.question').hide();
+  $('.correct-msg').hide();
+  $('.incorrect-msg').hide();
+  $('#lose-game-msg').hide();
+
+  /********************************************************
+  Dashboard / Welcome Screen
+  ********************************************************/
+
+  // Allow user to type name immediately on page load
+  $('#player-name').focus();
+
+    // When user submits name, welcome screen disappears
+  function removeWelcomeScreen() {
+    $('#welcome-screen').detach();
+    $('#dashboard').show().addClass('animated fadeIn');
+    $('#question1').show().addClass('animated fadeIn').focus();
+  };
+
+  // Store player name
+  $('#name-button').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerName = $('#player-name').val();
+    console.log(playerName);
+    $('#show-player-name').text(playerName);
+    $('#welcome-screen').addClass('animated fadeOut');
+    $('#tries-left').text(triesLeft);
+    setTimeout(removeWelcomeScreen, 1000);
+  });
+
+  /********************************************************
+  QUESTION 1
+  ********************************************************/
+
+  // Store answer 1
+  $('#question1-btn').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerAnswer1 = $('#input-question1').val().trim().toLowerCase(); // This assigns the user's input to the variable playerAnswer1
+    console.log('The players answer for question 1 is ' + playerAnswer1);
+    // showPlayerAnswer1(); // This first showPlayerAnswer1 function
+    answerCheck1();
+  });
+
+  // Show answers
+  function showPlayerAnswer1() {
+    $('#show-player-answer1').text(playerAnswer1);
+    $('#correct-answer1').text(answer1)
+  };
+
+  // Fade out answer check message
+  function fadeOutAnswerCheckMsg() {
+    $('#incorrect-msg1').removeClass('fadeIn').addClass('fadeOut');
+  };
+
+  function fadeOutQuestion1() {
+    $('#question1').addClass('animated fadeOut');
+    setTimeout($('#question1').detach(), 2500);
+    $('#question2').show().addClass('animated fadeIn');
+  };
+
+  function triesLeftCountdown() {
+    triesLeft--;
+    $('#tries-left').text(triesLeft);
+    if(triesLeft === 0) {
+      $('.question').addClass('animated fadeOut');
+      $('.question').detach();
+      $('#lose-game-msg').show().addClass('animated fadeIn')
+    };
+  };
+
+  function answerCheck1() {
+    // If player gets question right...
+    if(playerAnswer1 === answer1) {
+      console.log("Player's answer is correct!");
+      $('#correct-msg1').show().addClass('animated fadeIn');
+      correctCounter++;
+      $('#score').text(correctCounter);
+      setTimeout(fadeOutQuestion1, 2500);
+    }
+    else {
+      // If player gets question wrong...
+      console.log("Player's answer is incorrect!");
+      $('#incorrect-msg1').show().addClass('animated fadeIn');
+      triesLeftCountdown();
+      setTimeout(fadeOutAnswerCheckMsg, 2500);
+    }
+  };
+
+  /********************************************************
+  QUESTION 2
+  ********************************************************/
+
+  // Store answer 2
+  $('#question2-btn').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerAnswer2 = $('#input-question2').val().trim().toLowerCase(); // This assigns the user's input to the variable playerAnswer1
+    console.log('The players answer for question 2 is ' + playerAnswer2);
+    // showPlayerAnswer1(); // This first showPlayerAnswer1 function
+    answerCheck2();
+  });
+
+  // Show answers
+  function showPlayerAnswer2() {
+    $('#show-player-answer2').text(playerAnswer2);
+    $('#correct-answer2').text(answer2)
+  };
+
+  // Fade out answer check message
+  function fadeOutAnswerCheckMsg() {
+    $('#incorrect-msg2').removeClass('fadeIn').addClass('fadeOut');
+  };
+
+  function fadeOutQuestion2() {
+    $('#question2').addClass('animated fadeOut');
+    setTimeout($('#question2').detach(), 2500);
+    $('#question3').show().addClass('animated fadeIn');
+  };
+
+  function answerCheck2() {
+    // If player gets question right...
+    if(playerAnswer2 === answer2) {
+      console.log("Player's answer is correct!");
+      $('#correct-msg2').show().addClass('animated fadeIn');
+      correctCounter++;
+      $('#score').text(correctCounter);
+      setTimeout(fadeOutQuestion2, 2500);
+    }
+    else {
+      // If player gets question wrong...
+      console.log("Player's answer is incorrect!");
+      $('#incorrect-msg2').show().addClass('animated fadeIn');
+      setTimeout(fadeOutAnswerCheckMsg, 2500);
+    }
+  };
+
+}); // ready function ends
