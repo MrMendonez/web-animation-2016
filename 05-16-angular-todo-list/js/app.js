035c65ecba6fbb9db91e4fe3f3d708a9dcabe4ac
MrMendonez <dmendonez@gmail.com>
2016-05-16 10:01:56 -0400

    fixed delete btn and added css to done or not done tasks

--- 05-16-angular-todo-list/js/app.js
+++ 05-16-angular-todo-list/js/app.js
@@ -2,10 +2,10 @@ angular.module("todoApp", [])
 .controller("TodoListController", function($scope) {
   var todoList = this;
   todoList.todoItems = [
-    { name: "Go to school" },
-    { name: "Eat lunch" },
-    { name: "Do laundry" },
-    { name: "Buy materials for project" }
+    { name: "Go to school", done: false },
+    { name: "Eat lunch", done: false },
+    { name: "Do laundry", done: false },
+    { name: "Buy materials for project", done: false }
   ]
 
   todoList.addTodo = function() {
